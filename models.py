e5b9d6aaca70c9639ce0b814ce71746fc0bd9172
Juliette Lonij <jlonij@gmail.com>
2016-12-19 19:23:17 +0100

    Add license and readme

--- models.py
+++ models.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# Frame Generator
+#
+# Copyright (C) 2016 Juliette Lonij, Koninklijke Bibliotheek -
+# National Library of the Netherlands
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import unicodecsv as csv
 import gensim
